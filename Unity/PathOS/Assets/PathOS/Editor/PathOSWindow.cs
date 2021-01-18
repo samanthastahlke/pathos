b3919fb126fee7e385d8b08549a5fdc5d7382d0b
AtiyaNova <atiyanova@gmail.com>
2021-01-18 04:05:21 -0500

    Beginnings of the PathOS Window

---
+++ Unity/PathOS/Assets/PathOS/Editor/PathOSWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/*
+ * Atiya Nova 2021
+ */
+
+public class PathOSWindow : EditorWindow
+{
+
+    string[] tabLabels = { "Agent", "Manager", "Batching" };
+    int tabSelection = 0;
+
+    [MenuItem("Window/PathOS")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(PathOSWindow));
+    }
+
+    void OnGUI()
+    {
+        // The actual window code goes here
+        GUILayout.BeginHorizontal();
+        tabSelection = GUILayout.Toolbar(tabSelection, tabLabels);
+        GUILayout.EndHorizontal();
+    }
+}
